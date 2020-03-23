4c69c39cbf37041734af1fbb93e6bfcbab6b9616
dunja <dunjavulovic95@gmail.com>
2020-03-23 10:19:59 +0100

    classes are updated

--- PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectService.java
@@ -1,8 +1,18 @@
 package com.dvulovic.ppmtool.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.dvulovic.ppmtool.domain.Project;
+import com.dvulovic.ppmtool.repository.ProjectRepository;
 
 @Service
 public class ProjectService {
 	
+	@Autowired
+	ProjectRepository projectRepository;
+	
+	public Project saveOrUpdateProject(Project project) {
+		return projectRepository.save(project);
+	}
 }
