5656760d20dcfcf3ef71fa8eaf5a2a75eb940b6d
dunja <dunjavulovic95@gmail.com>
2020-03-25 11:09:58 +0100

    added new components

--- ppmtool-react/src/components/Dashboard.js
+++ ppmtool-react/src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
   render() {
-    return <h1>Dashboard</h1>;
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <ProjectItem />
+      </div>
+    );
   }
 }
 export default Dashboard;
