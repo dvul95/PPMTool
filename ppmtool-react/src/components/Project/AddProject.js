07996fd1ab67fa1e98682d8510a1aa5ba6604f1b
dunja <dunjavulovic95@gmail.com>
2020-03-26 11:26:27 +0100

    adding input form

--- ppmtool-react/src/components/Project/AddProject.js
+++ ppmtool-react/src/components/Project/AddProject.js
@@ -1,10 +1,81 @@
 import React, { Component } from "react";
 
 class AddProject extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projectName: "",
+      projectIdentifier: "",
+      projectDescription: "",
+      startDate: "",
+      endDate: ""
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1>Add Project Form</h1>
+        <div className="project">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h5 className="display-4 text-center">Create Project Form</h5>
+                <hr />
+                <form>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg "
+                      placeholder="Project Name"
+                      name="projectName"
+                      value={this.state.projectName}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Unique Project ID"
+                      name="projectIdentifier"
+                      value={this.state.projectIdentifier}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      className="form-control form-control-lg"
+                      placeholder="Project Description"
+                      name="projectDescription"
+                      value={this.state.projectDescription}
+                    ></textarea>
+                  </div>
+                  <h6>Start Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="startDate"
+                      value={this.state.startDate}
+                    />
+                  </div>
+                  <h6>Estimated End Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="endDate"
+                      value={this.state.endDate}
+                    />
+                  </div>
+
+                  <input
+                    type="submit"
+                    className="btn btn-primary btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
