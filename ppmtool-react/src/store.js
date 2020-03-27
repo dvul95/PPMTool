94b53a2c14e86288ab82676c258d3b7ebde0a42d
dunja <dunjavulovic95@gmail.com>
2020-03-27 09:39:04 +0100

    updating store.js

--- ppmtool-react/src/store.js
+++ ppmtool-react/src/store.js
@@ -1,3 +1,30 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducers from "./reducers";
+import rootReducer from "./reducers";
+
+const initialState = {};
+const middleware = [thunk];
+
+let store;
+
+//if it is chrome....
+if (window.navigator.userAgent.includes("Chrome")) {
+  //creates the store
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+}
+
+export default store;
