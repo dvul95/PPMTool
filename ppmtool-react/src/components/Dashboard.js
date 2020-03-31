418467417dbfbbe21ded09d706840080ad86224d
dunja <dunjavulovic95@gmail.com>
2020-03-31 11:32:03 +0200

    props added

--- ppmtool-react/src/components/Dashboard.js
+++ ppmtool-react/src/components/Dashboard.js
@@ -12,6 +12,7 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { projects } = this.props.project;
     return (
       <div className="projects">
         <div className="container">
@@ -22,7 +23,9 @@ class Dashboard extends Component {
               <CreateButton />
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
