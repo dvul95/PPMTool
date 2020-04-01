c9df5b73837a55806c96e85b7cd85c80c863efd7
dunja <dunjavulovic95@gmail.com>
2020-04-01 09:30:36 +0200

    added a getproject component

--- ppmtool-react/src/components/Project/UpdateProject.js
+++ ppmtool-react/src/components/Project/UpdateProject.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
+import { getProject } from "../../actions/ProjectActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
 
 class UpdateProject extends Component {
+  //Lifecycle hook
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
+
   render() {
     return (
       <div className="project">
@@ -60,4 +70,14 @@ class UpdateProject extends Component {
     );
   }
 }
-export default UpdateProject;
+
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project.project
+});
+
+export default connect(mapStateToProps, { getProject })(UpdateProject);
