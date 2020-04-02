2b11f8a6f13033b0afe4e34a9cd1ec514ff29065
dunja <dunjavulovic95@gmail.com>
2020-04-02 09:42:58 +0200

    added delete project function

--- ppmtool-react/src/actions/ProjectActions.js
+++ ppmtool-react/src/actions/ProjectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 //create Projects
 export const createProject = (project, history) => async dispatch => {
@@ -39,4 +39,13 @@ export const getProject = (id, history) => async dispatch => {
   } catch (error) {
     history.push("/dashboard");
   }
+};
+
+//delete Project
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
 };
