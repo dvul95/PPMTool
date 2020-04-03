7079f095b50f6b4129236aaeeb6ece893ede544c
dunja <dunjavulovic95@gmail.com>
2020-04-03 10:46:26 +0200

    updating service class

--- PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectTaskService.java
@@ -3,6 +3,7 @@ package com.dvulovic.ppmtool.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dvulovic.ppmtool.domain.Backlog;
 import com.dvulovic.ppmtool.domain.ProjectTask;
 import com.dvulovic.ppmtool.repository.BacklogRepository;
 import com.dvulovic.ppmtool.repository.ProjectTaskRepository;
@@ -16,7 +17,26 @@ public class ProjectTaskService {
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
 	
-	//public ProjectTask addProjectTask() {
+	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+		projectTask.setBacklog(backlog);
 		
-	//}
+		Integer BacklogSequence = backlog.getPTSequence();
+
+		BacklogSequence++;
+		projectTask.setProjectSequence(projectIdentifier+"-"+ BacklogSequence);
+		projectTask.setProjectIdentifier(projectIdentifier);
+		
+		
+		if(projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+			projectTask.setPriority(3);
+		}
+		
+		if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+			projectTask.setStatus("TODO");
+		}
+		
+		return projectTaskRepository.save(projectTask);
+		
+	}
 }
