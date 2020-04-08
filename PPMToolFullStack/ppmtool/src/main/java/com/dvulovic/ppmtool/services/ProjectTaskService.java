2abc8f43fa1c8daedbade1a1f186bbcdef340f4a
dunja <dunjavulovic95@gmail.com>
2020-04-08 10:46:04 +0200

    updated class

--- PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectTaskService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.dvulovic.ppmtool.domain.Backlog;
 import com.dvulovic.ppmtool.domain.ProjectTask;
 import com.dvulovic.ppmtool.exceptions.ProjectIdException;
+import com.dvulovic.ppmtool.exceptions.ProjectNotFoundException;
 import com.dvulovic.ppmtool.repository.BacklogRepository;
 import com.dvulovic.ppmtool.repository.ProjectTaskRepository;
 
@@ -16,31 +17,35 @@ public class ProjectTaskService {
 
 	@Autowired
 	private BacklogRepository backlogRepository;
-	
+
 	@Autowired
 	private ProjectTaskRepository projectTaskRepository;
-	
+
 	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-	
+
+		try {
 			Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-			projectTask.setBacklog(backlog);	
+			projectTask.setBacklog(backlog);
 			Integer BacklogSequence = backlog.getPTSequence();
 			BacklogSequence++;
 			backlog.setPTSequence(BacklogSequence);
-			projectTask.setProjectSequence(projectIdentifier+"-"+ BacklogSequence);
+			projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
 			projectTask.setProjectIdentifier(projectIdentifier);
-			
-		
-		if( projectTask.getPriority() == null) {
-			projectTask.setPriority(3);
+
+			if (projectTask.getPriority() == null) {
+				projectTask.setPriority(3);
+			}
+
+			if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+				projectTask.setStatus("TODO");
+			}
+
+			return projectTaskRepository.save(projectTask);
+
+		} catch (Exception e) {
+			throw new ProjectNotFoundException("Project not found.");
 		}
-		
-		if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
-			projectTask.setStatus("TODO");
-		}
-		
-		return projectTaskRepository.save(projectTask);
-		
+
 	}
 
 	public Iterable<ProjectTask> findBacklogById(String backlog_id) {
