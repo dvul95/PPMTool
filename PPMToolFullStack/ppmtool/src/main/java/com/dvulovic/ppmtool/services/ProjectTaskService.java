c467f933c3e59f5ec7731612422692ff6f169379
dunja <dunjavulovic95@gmail.com>
2020-04-08 10:07:02 +0200

    added new error handling class

--- PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/ProjectTaskService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.dvulovic.ppmtool.domain.Backlog;
 import com.dvulovic.ppmtool.domain.ProjectTask;
+import com.dvulovic.ppmtool.exceptions.ProjectIdException;
 import com.dvulovic.ppmtool.repository.BacklogRepository;
 import com.dvulovic.ppmtool.repository.ProjectTaskRepository;
 
@@ -20,18 +21,15 @@ public class ProjectTaskService {
 	private ProjectTaskRepository projectTaskRepository;
 	
 	public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
-		Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-		projectTask.setBacklog(backlog);
-		
-		Integer BacklogSequence = backlog.getPTSequence();
-
-		BacklogSequence++;
-		
-		backlog.setPTSequence(BacklogSequence);
-		
-		projectTask.setProjectSequence(projectIdentifier+"-"+ BacklogSequence);
-		projectTask.setProjectIdentifier(projectIdentifier);
-		
+	
+			Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+			projectTask.setBacklog(backlog);	
+			Integer BacklogSequence = backlog.getPTSequence();
+			BacklogSequence++;
+			backlog.setPTSequence(BacklogSequence);
+			projectTask.setProjectSequence(projectIdentifier+"-"+ BacklogSequence);
+			projectTask.setProjectIdentifier(projectIdentifier);
+			
 		
 		if( projectTask.getPriority() == null) {
 			projectTask.setPriority(3);
