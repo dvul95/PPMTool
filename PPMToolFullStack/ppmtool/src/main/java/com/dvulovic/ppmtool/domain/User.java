5f119aa5ac891d27393cae6f6d2761f24ab76dce
dunja <dunjavulovic95@gmail.com>
2020-04-23 09:30:52 +0200

    implemented user methods

--- PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/domain/User.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/domain/User.java
@@ -1,5 +1,6 @@
 package com.dvulovic.ppmtool.domain;
 
+import java.util.Collection;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -13,8 +14,11 @@ import javax.persistence.Transient;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class User {
+public class User implements UserDetails{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -102,6 +106,33 @@ public class User {
 	@PreUpdate
 	protected void onUpdate() {
 		this.updatedAt = new Date();
+	}
+	
+	/* UserDetails methods */
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
