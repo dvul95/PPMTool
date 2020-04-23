a5f5a64752266c0b59a7a3292db51da9768bb518
dunja <dunjavulovic95@gmail.com>
2020-04-23 10:07:10 +0200

    update user classes, update security options for password

--- PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/UserService.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/services/UserService.java
@@ -1,8 +1,10 @@
 package com.dvulovic.ppmtool.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.dvulovic.ppmtool.domain.User;
 import com.dvulovic.ppmtool.repository.UserRepository;
 
 @Service
@@ -10,4 +12,13 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public User saveUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		
+		return userRepository.save(user);
+	}
 }
