8382b29096584b52a4c89eadc990204ffdd3f046
dunja <dunjavulovic95@gmail.com>
2020-04-24 14:13:31 +0200

    created new class, updated old and new class for user authentication

--- PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/repository/UserRepository.java
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/repository/UserRepository.java
@@ -7,5 +7,7 @@ import com.dvulovic.ppmtool.domain.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
-
+	
+	User findByUsername(String username);
+	User getById(Long id);
 }
