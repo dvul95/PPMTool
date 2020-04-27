1e3a38d17e175444568731ee857d8ec931a52465
dunja <dunjavulovic95@gmail.com>
2020-04-27 09:31:23 +0200

    created method to generate token

---
+++ PPMToolFullStack/ppmtool/src/main/java/com/dvulovic/ppmtool/security/JwtTokenProvider.java
@@ -0,0 +1,37 @@
+package com.dvulovic.ppmtool.security;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import static com.dvulovic.ppmtool.security.SecurityConstants.EXPIRATION_TIME;
+import static com.dvulovic.ppmtool.security.SecurityConstants.SECRET;
+
+import com.dvulovic.ppmtool.domain.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtTokenProvider {
+	
+	//Method to generate the token
+
+	public String generateToken(Authentication authentication) {
+		User user = (User) authentication.getPrincipal();
+
+		Date now = new Date(System.currentTimeMillis());
+		Date expirationDate = new Date(now.getTime() + EXPIRATION_TIME);
+
+		String userId = Long.toString(user.getId());
+
+		Map<String, Object> claims = new HashMap<>();
+		claims.put("id", (Long.toString(user.getId())));
+		claims.put("username", user.getUsername());
+		claims.put("fullName", user.getFullName());
+
+		return Jwts.builder().setSubject(userId).setClaims(claims).setIssuedAt(now).setExpiration(expirationDate)
+				.signWith(SignatureAlgorithm.HS512, SECRET).compact();
+	}
+	// --------------------------------------------------------------
+}
